7e9b5389dc91e4af5de8e82f85f41e6857d00532
初志鑫 <1204505056@qq.com>
2020-04-20 18:50:40 +0800

    更新ie下温馨提示

--- src/plugins/support.js
+++ src/plugins/support.js
@@ -4,9 +4,9 @@ if (!!window.ActiveXObject || "ActiveXObject" in window) {
   MessageBox({
     title: "温馨提示",
     message:
-      '自2015年3月起,微软已宣布弃用IE,且不再对IE提供任何更新维护,请访问<a target="_blank" style="color:blue" href="https://www.microsoftedgeinsider.com/zh-cn/download/">https://www.microsoftedgeinsider.com/zh-cn/download/</a>' +
-      "下载最新版访问该系统,如果您使用的是双核浏览器,请您切换浏览器内核为极速模式",
+      '自2015年3月起，微软已宣布弃用IE，且不再对IE提供任何更新维护，请<a target="_blank" style="color:blue" href="https://www.microsoft.com/zh-cn/edge/">点击此处</a>访问微软官网更新浏览器，如果您使用的是双核浏览器,请您切换浏览器内核为极速模式',
     type: "warning",
+    showClose: false,
     showConfirmButton: false,
     closeOnClickModal: false,
     closeOnPressEscape: false,
