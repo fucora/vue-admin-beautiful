65e5f7cfd05001469719ef4eabe63a3d4620bc04
初志鑫 <1204505056@qq.com>
2020-04-21 17:02:38 +0800

    公共配置优化

--- .eslintrc.js
+++ .eslintrc.js
@@ -12,8 +12,8 @@ module.exports = {
     echarts: false,
   },
   rules: {
-    "no-console": process.env.NODE_ENV === "production" ? "error" : "off",
-    "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off",
+    "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
+    "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
     "vue/no-v-html": "off",
   },
   parserOptions: {
